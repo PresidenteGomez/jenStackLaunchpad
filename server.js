15be1a64e43ec4792c5d266f1675369722c3b8bf
Nestor Gomez Jimenez <gomez315@umn.edu>
2017-09-06 16:03:07 -0500

    second iteration of changes

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ var path = require( 'path' );
 var port = 3000;
 var app = express();
 
+//jen-calls being counted on the terminal with console.log
+var jenCount = 0;
+//return an object from the server that has the number of 
+//times the route has been called
+var jenObject = {
+  calls: 0
+};
+
 app.use( express.static( 'public' ) );
 
 // spin up server
@@ -21,5 +29,14 @@ app.get( '/', function( req, res ){
 app.get('/jen', function (req, res) {
   // this is the root url
   console.log('request for Jen received');
-  res.send('jen request');
+  jenCount += 1;
+  jenObject.calls = jenCount;
+  console.log('Jen-Call Counts:', jenCount);
+
+
+
+  res.send(jenObject);
   });
+  
+  // create a counter in server.js that counts how many times 
+  //the get route has been called and show in terminal with console.log
